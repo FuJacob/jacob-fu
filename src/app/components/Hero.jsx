b57b93ab4b23f83b4cb6483024d2743aab4a43f0
Jacob Fu <jjacobfu@gmail.com>
2024-11-12 03:34:24 -0500

    added components

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello!</h1>
+          <h2 className="text-5xl font-bold py-12">This is Jacob Fu</h2>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
